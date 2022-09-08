30567b26a7cbef23e49210323ffe024965f51e48
Phuong Nguyen <phuongsover2@gmail.com>
2022-09-08 17:01:43 +0700

    Event Propagation in practice.

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -80,3 +80,30 @@ h1.addEventListener('mouseenter', alertH1);
 // h1.onmouseenter = function(e) {
 // 	alert('onmouseenter: Great! You are reading the heading :D');
 // };
+
+
+
+// Event Propagation
+const randomInt = (min, max) =>
+	Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () => `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+console.log(randomColor());
+
+document.querySelector('.nav__link').addEventListener('click', function(e) {
+	console.log('LINK', e.target, e.currentTarget);
+	console.log(e.currentTarget === this);
+
+	// Stop propagation
+	// e.stopPropagation();
+});
+
+
+document.querySelector('.nav__links').addEventListener('click', function(e) {
+	console.log('CONTAINER', e.target, e.currentTarget);
+});
+
+
+document.querySelector('.nav').addEventListener('click', function(e) {
+	console.log('NAV', e.target, e.currentTarget);
+}, true);

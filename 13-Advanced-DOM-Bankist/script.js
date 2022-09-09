3a27357375185487eab7740e41b118763f278680
Phuong Nguyen <phuongsover2@gmail.com>
2022-09-09 20:51:02 +0700

    Implemented lazy-load on image for performance.

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -206,3 +206,29 @@ allSections.forEach(function(section) {
 	sectionObserver.observe(section);
 	section.classList.add('section--hidden');
 });
+
+
+// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]');
+console.log(imgTargets);
+
+const loadImg = function(entries, observer) {
+	const [entry] = entries;
+	if (!entry.isIntersecting) return;
+
+	// Replace src with data-src
+	entry.target.src = entry.target.dataset.src;
+
+	entry.target.addEventListener('load', function() {
+		entry.target.classList.remove('lazy-img');
+	});
+
+	observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+	root: null,
+	threshold: 0,
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));

9e976e97484c6d2526ad7321372414c672f15b16
Phuong Nguyen <phuongsover2@gmail.com>
2022-09-09 11:04:46 +0700

    Implemented Changing opacity when hover a link in navbar.

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -7,10 +7,15 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 // Implement smooth scrolling
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 
 const section1 = document.querySelector('#section--1');
+
+const nav = document.querySelector('.nav');
 
 const openModal = function(e) {
 	e.preventDefault();
@@ -87,9 +92,6 @@ document.querySelector('.nav__links').addEventListener('click',
 	});
 
 // Tabbed component
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
 
 tabsContainer.addEventListener('click', function(e) {
 	e.preventDefault();
@@ -106,4 +108,29 @@ tabsContainer.addEventListener('click', function(e) {
 	tabsContent.forEach(tab => tab.classList.remove('operations__content--active'));
 
 	document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
+});
+
+// Menu fade animation 
+// Refactoring 
+const handleHover = function(e, opacity) {
+
+	const link = e.target;
+	const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+	const logo = link.closest('.nav').querySelector('img');
+
+	siblings.forEach(el => {
+		if (el !== link) el.style.opacity = opacity;
+	});
+
+	logo.style.opacity = opacity;
+};
+
+nav.addEventListener('mouseover', function(e) {
+	if (e.target.classList.contains('nav__link')) {
+		handleHover(e, 0.5);
+	}
+});
+
+nav.addEventListener('mouseout', function(e) {
+	handleHover(e, 1);
 });

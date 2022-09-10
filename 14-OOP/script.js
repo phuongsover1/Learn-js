2971133390a0e9187c90881ada3315f7d89fdadb
Phuong Nguyen <phuongsover2@gmail.com>
2022-09-10 17:09:49 +0700

    Learned how to declare a class in ES6.

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -26,3 +26,84 @@ const jack = 'jack';
 console.log(jonas instanceof Person);
 console.log(matilda instanceof Person);
 console.log(jack instanceof Person);
+
+// Prototypes
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function() {
+	console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+console.log(jonas);
+
+// Print the prototype of object
+// console.log(jonas.__proto__);
+// console.log(jonas.__proto__ === Person.prototype);
+
+// console.log(Person.prototype.isPrototypeOf(jonas));
+// console.log(Person.prototype.isPrototypeOf(matilda));
+// console.log(Person.prototype.isPrototypeOf(Person));
+
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
+
+// class expression
+// const PersonCl = class {
+//
+// };
+
+
+// class Declaration
+class PersonCl {
+	constructor(firstName, birthYear) {
+		this.firstName = firstName;
+		this.birthYear = birthYear;
+
+		this.doSomething = function() {
+			console.log('doSomething');
+		};
+	}
+
+	// Methods will be added to .prototype property
+	calcAge() {
+		console.log(2037 - this.birthYear);
+	}
+}
+
+const jessica = new PersonCl('jessica', 1996);
+
+console.log(jessica);
+
+PersonCl.prototype.greet = function() {
+	console.log(`Hey ${this.firstName}`);
+};
+
+
+jessica.greet();
+
+// // Setter and getter in regular object 
+// const account = {
+// 	owner: 'Jonas',
+// 	movements: [200, 540, 120, 300],
+//
+// 	get latest() {
+// 		return this.movements.slice(-1).pop();
+// 	},
+//
+// 	set latest(mov) {
+// 		this.movements.push(mov);
+// 	}
+// };
+//
+//
+// console.log(account.latest);
+//
+// console.log('---- Setter ----');
+// account.latest = 5000;
+// console.log(account);
+//

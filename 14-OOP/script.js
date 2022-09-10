cbe43d221e91fe9c17a21fe76b4250a8109b04bb
Phuong Nguyen <phuongsover2@gmail.com>
2022-09-10 18:03:05 +0700

    Learned declaring getter,setter in class.

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -58,10 +58,31 @@ console.log(jonas.hasOwnProperty('species'));
 // };
 
 
+// Setter and getter in regular object 
+const account = {
+	owner: 'Jonas',
+	movements: [200, 540, 120, 300],
+
+	get latest() {
+		return this.movements.slice(-1).pop();
+	},
+
+	set latest(mov) {
+		this.movements.push(mov);
+	}
+};
+
+
+console.log(account.latest);
+
+console.log('---- Setter ----');
+account.latest = 5000;
+console.log(account);
+
 // class Declaration
 class PersonCl {
-	constructor(firstName, birthYear) {
-		this.firstName = firstName;
+	constructor(fullName, birthYear) {
+		this.fullName = fullName;
 		this.birthYear = birthYear;
 
 		this.doSomething = function() {
@@ -73,37 +94,30 @@ class PersonCl {
 	calcAge() {
 		console.log(2037 - this.birthYear);
 	}
+
+	get age() {
+		return 2037 - this.birthYear;
+	}
+
+	// Set a property that already exists
+	set fullName(name) {
+		console.log(name);
+		if (name.includes(' ')) this._fullName = name;
+		else alert('`${name}` is not a full name!');
+	}
+
+	get fullName() {
+		return this._fullName;
+	}
+
 }
 
-const jessica = new PersonCl('jessica', 1996);
-
+const jessica = new PersonCl('Jessica David', 1870);
 console.log(jessica);
 
-PersonCl.prototype.greet = function() {
-	console.log(`Hey ${this.firstName}`);
-};
 
+console.log('---- Getter ----');
+console.log(jessica.fullName);
 
-jessica.greet();
-
-// // Setter and getter in regular object 
-// const account = {
-// 	owner: 'Jonas',
-// 	movements: [200, 540, 120, 300],
-//
-// 	get latest() {
-// 		return this.movements.slice(-1).pop();
-// 	},
-//
-// 	set latest(mov) {
-// 		this.movements.push(mov);
-// 	}
-// };
-//
-//
-// console.log(account.latest);
-//
-// console.log('---- Setter ----');
-// account.latest = 5000;
-// console.log(account);
-//
+// console.log(Number.parseFloat('1.2'));
+// Number(3).parseFloat();
